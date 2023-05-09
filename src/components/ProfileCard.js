548663b0910cad566223857e2bf65afa2f4d45b7
Mustafakudin <m.kudin1999@gmail.com>
2023-05-09 21:01:44 +0300

    debugGiderildi

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -12,15 +12,19 @@ import ButtonWithProgress from './ButtonWithProgress';
                                     // iki veriye ihtiyacımız var birinicis login olan kullanıcı bilgisi ikincisi ise biz suan hangi sayfadayız yani login olan kullanıcıyla bulunudugmuz sayfa birbirine eşit ise  o zaman biz suan kullanıcın login olan sayfasındayız ve editleyebiliriz 
       const[inEditMode , setInEditMode] =useState(false);
       const[updateDisplayName ,setUpdateDisplayName] = useState();
-      const {username : loggedInUsername} = useSelector((store) => ({
-        username : store.username
-      }))
+      const {username : loggedInUsername} = useSelector((store) => ({username : store.username}))
       const routeParams=useParams();
+      const pathUsername=routeParams.username; 
       const [user,setUser]  =useState({});
+      const [editable ,setEditable] = useState(false);
       useEffect(() =>{
         setUser(props.user)
       },[props.user]) 
-    //  const{user} = props;
+      useEffect(() =>{
+        setEditable(pathUsername === loggedInUsername)
+       
+      },[pathUsername,loggedInUsername])
+
       const{username,displayName,image} = user;
       const{t} = useTranslation();
       
@@ -47,10 +51,10 @@ import ButtonWithProgress from './ButtonWithProgress';
       
       
       const pendingApiCall  =  useApiProgress['put', '/api/1.0/users/' + username]
-      const pathUsername=routeParams.username;  // burada sunu yaptık components yani browserdaki props kazıya kazıya en yukardan en aşaıga ulaştık 
+     // const pathUsername=routeParams.username;  // burada sunu yaptık components yani browserdaki props kazıya kazıya en yukardan en aşaıga ulaştık 
                           // const loggedInUsername=props.username;   //  value içinden getirebiliriz auten sınıfda vlaue onun içinde state statte yukarda username vs fieldları tutuyor    bunu yapmamızın amacı login olduktan sonra user1 tıkladıgımızda we can edit yazabilsin diye  
        
-      const editable = pathUsername === loggedInUsername;
+      
       return(
         <div className='card text-center'>
           <div className='card-header'>
